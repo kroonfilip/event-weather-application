0f3ce3bfe3ee1213e83a0310d366772a27ecf7bc
Cassandrawindahl <cassandrawindahl@outlook.com>
2022-05-19 12:38:17 +0200

    Implemented API openWeather
    The openWeather works now, we just need to implement it to the React app. I have just fixed so that the calls work - there is not much logic in it.

--- APIs/api.js
+++ APIs/api.js
@@ -1,11 +1,11 @@
-/*
+
 import { ticketmaster, openWeather } from './safety.js' //Detta funkar inte...
 
 
 //Ticketmaster - tar ut 20 *senaste* eventen i en specifik stad.  
 $.ajax({ //Gör en request till API:et
     type: 'GET',
-    url: 'https://app.ticketmaster.com/discovery/v2/events.json?city=Malmo&apikey='+{ticketmaster},
+    url: 'https://app.ticketmaster.com/discovery/v2/events.json?city=Malmo&apikey='+ticketmaster,
     async: true,
     dataType: "json",
     success: function(data){
@@ -16,20 +16,31 @@ $.ajax({ //Gör en request till API:et
 
     }
 })
-*/
 
 //OpenWeather - Geocoding call
-let place = 'London'
+let place = 'Malmo'
+
 $.ajax({ //Gör en request till API:et
     type: 'GET',
-    url: 'http://api.openweathermap.org/geo/1.0/direct?q='+{place}+'&'+{openWeather},
-    dataType: "json",
+    url: 'http://api.openweathermap.org/geo/1.0/direct?q='+place+'&appid='+openWeather,
     success: function(data){
-        console.log(data)
+        $.each(data, function(i, item) {
+            let lat = item.lat
+            let long = item.lon
+            $.ajax({
+                type: 'GET',
+                url: 'http://api.openweathermap.org/data/2.5/forecast?lat='+lat+'&lon='+long+'&appid='+openWeather,
+                success: function(data){
+                    console.log(data.list)
+                },
+                error: function(error){
+                    console.log(error)
+                }
+            })
+        })
     }, 
     error: function(error) { //Hanterar om vi får error av API:et. 
         console.log(error)
-
     }
 })
 
