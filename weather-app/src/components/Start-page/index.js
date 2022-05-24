56cd04121f481457528bc07da1ca8e3185319f33
Cassandrawindahl <cassandrawindahl@outlook.com>
2022-05-24 17:49:35 +0200

    Changed logic on startPage
    The code did not work as it should on the startPage so we changed that. The idea from the beginning didn't work so we had to change so we only get the weather for one specific day.
    
    We also changed so that the code have different functions instead of having all the things it does under one function.
    
    Co-Authored-By: kroonfilip <81237654+kroonfilip@users.noreply.github.com>

--- weather-app/src/components/Start-page/index.js
+++ weather-app/src/components/Start-page/index.js
@@ -1,46 +1,82 @@
-import React, {useState} from 'react'
+import React, {useState, useRef} from 'react'
 import axios from 'axios'
 
 const Startpage = () => {
     
     const [data, setData] = useState([])
-    const [location, setLocation] = useState('')
-    let place = 'Malmö'
-    const apiUrlGeoLocation = 'http://api.openweathermap.org/geo/1.0/direct?q='+location+'&appid='
+    const location = useRef()
+    const date = useRef()
+    
+    const dateToday = new Date()
+
+    const endDate = new Date()
+    endDate.setDate(endDate.getDate() + 7);
+    console.log(endDate)
+
+    const dateChoosen = '2022-05-26T11:00:00Z'
+
 
     const searchFunction = (event) => {
-        if (event.key === 'Enter') {
+        event.preventDefault()
+        if (location.current.value !=="" && date.current.value !== ""){
+            const apiUrlGeoLocation = 'http://api.openweathermap.org/geo/1.0/direct?q='+location.current.value+'&appid=bcea789825d8474a842b9612811b70e3'
             axios.get(apiUrlGeoLocation).then((response) => {
                 var lat = response.data[0].lat
                 var long = response.data[0].lon
-                const apiUrlWeather = 'https://api.openweathermap.org/data/3.0/onecall?lat='+lat+'&lon='+long+'&units=metric&lang=se&exclude=hourly,minutely&appid='
-                axios.get(apiUrlWeather).then((response) => {
-                    setData(response.data)
-                    console.log(response.data)
-                })
+                getWeather(lat, long, dateChoosen)  
             })
-            const apiUrlTicketmaster = 'https://app.ticketmaster.com/discovery/v2/events.json?city='+location+'&apikey='
+            /*const apiUrlTicketmaster = 'https://app.ticketmaster.com/discovery/v2/events.json?city='+location.current.value+'&startDateTime='+startDateWithTime+'&endDateTime='+endDateWithTime+'&apikey=4Kl2lBFXuu3mkGzmE4P6VXRoXqfgar8O'
             axios.get(apiUrlTicketmaster).then((answer) => {
+                //const events = answer.data._embedded.events;
                 console.log(answer.data)
             })
-        
-    }
-}       
+            */
+
+        } else {
+            alert("You need to give location and date to be able to get events!")
+        }
+
+        location.current.value= "";
+        date.current.value = "";
+}
+
+function getWeather(lat, long, dateChoosen) {
+    const apiUrlWeather = 'https://api.openweathermap.org/data/3.0/onecall?lat='+lat+'&lon='+long+'&units=metric&lang=en&exclude=hourly,minutely&appid=7b876dba81adf23c3ab28f297a4ac7aa'
+    axios.get(apiUrlWeather).then((response) => {
+        const DiffDays = getDiffDays('2022-05-26')
+        var weatherDaily = response.data.daily[DiffDays]
+        console.log(weatherDaily)
+        setData(weatherDaily)
+})}
+
+function getDiffDays(otherDate) {
+    const dateNew = new Date(otherDate)
+    let difference = dateNew.getTime() - dateToday.getTime();
+    var DifferenceInDays = difference / (1000 * 3600 * 24);
+    return Math.ceil(DifferenceInDays);
+
+}
+
+    var img = '{<img src={`http://openweathermap.org/img/w/${data.weather.icon}.png`} alt="weather icon"></img>}'
     
     var days = {weekday: 'long', month: 'long', day: 'numeric'};
-    
-    var renderApiDataForWeek = data.daily 
+
+    /*
+    var renderApiDataForWeek = data.daily
         ? data && data.daily.map(item =>{
                 return (
                 <React.Fragment key={item}>
-                <p>Datum: {new Date(item.dt * 1000).toLocaleDateString("se", days)}</p>
+                <div id={item.dt}>
+                <p>{new Date(item.dt * 1000).toLocaleDateString("en", days)}</p>
                 <img src = {`http://openweathermap.org/img/w/${data.current.weather[0].icon}.png`} alt="weather-icon"></img>
-                <p>Temperatur: {item.temp.day}°C</p>
+                <p>{item.temp.day.toFixed()}°C</p>
                 <p>{item.weather[0].description}</p>
+                </div>
                 
                 </React.Fragment>
                 )
             }) : "laddar..."
+            */
     return (
         <>
         <div className="start-page">
@@ -50,25 +86,29 @@ const Startpage = () => {
                 TEXT MED INFORMATION
             </p>
             <h3>Ange stad</h3>
+            <form action="#" onSubmit={searchFunction}>
             <input type="text" 
-            id ="search-field" 
-            onChange={event => setLocation(event.target.value)} 
-            value={location}
-            onKeyUp={searchFunction}
+            id ="location" ref={location} 
             placeholder="Sök stad"></input>
-            
+            <label for="start">Start date:</label>
+
+            <input type="date" id="date" ref={date}
+                min={dateToday} max={endDate}></input>
+            <button type="submit"  >
+                
+            </button>
+            </form>
             <h3>RESULTAT</h3>
             <div id="location-info">
                 <div id="current-weather">
-            
-                    {data.current ? <img src={`http://openweathermap.org/img/w/${data.current.weather[0].icon}.png`} alt="weather icon"></img>: null}
-                    {data.current ? <p>Temperatur: {data.current.temp}°C</p>: null}
-                    {data.current ? <p>Är just nu: {data.current.weather[0].description}</p>: null}
+                {data.temp ? <p> {data.temp.day.toFixed()}°C</p>:null}
+                {data.weather ?<p>{data.weather[0].description}</p>:null}
+                    {data.clouds}
                 </div>
 
                 <div id="weather-seven-days">
                 <h3>7 dagar frammåt</h3>
-                    {renderApiDataForWeek}
+                    
                 </div> 
             </div>
                 
