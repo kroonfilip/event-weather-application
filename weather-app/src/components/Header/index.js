8d0d33c899b997c0a35d29a074d7b67d705eb745
Dasa-ia-student <81235028+Dasa-ia-student@users.noreply.github.com>
2022-06-02 08:47:12 +0200

    Cleaning up some unused code
    Cleaning up some unused code

--- weather-app/src/components/Header/index.js
+++ weather-app/src/components/Header/index.js
@@ -1,11 +1,10 @@
-
 const Header = () => {
     return (
         <>
             <header>
                 <div className="w3-display-container w3-animate-opacity">
                     <div className="image-parent">
-                        <img className="sky" src="sky.jpg" alt="boat" style={{width: "100%", minHeight: 350, maxHeight: 600,}}/>
+                        <img className="sky" src="sky.jpg" alt="sky" style={{width: "100%", minHeight: 350, maxHeight: 600,}}/>
                         <img className="logo" src="logo.png" alt="logo" style={{minHeight: 175, maxHeight: 200,}}/>
                     </div>
                     <div className="w3-container w3-display-bottomleft w3-margin-bottom">  
--- weather-app/src/components/Menu/index.js
+++ weather-app/src/components/Menu/index.js
@@ -18,7 +18,7 @@ const Menu = () => {
             <div className="w3-top w3-black w3-padding-12">
                 <div className="w3-bar w3-theme-d3 w3-left-align">
                     <div className="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-hover-amber w3-theme-d2" onClick={openNav}><i class="fa fa-bars"></i></div>
-                    <span class="w3-right">
+                    <span className="w3-right">
                         <Link to="/" className="w3-bar-item w3-button w3-hide-small w3-hover-amber">Home</Link>
                         <Link to="/favorites" className="w3-bar-item w3-button w3-hide-small w3-hover-amber">Favorites</Link>
                         <a className="w3-bar-item w3-button w3-hide-small w3-hover-amber">About</a>
